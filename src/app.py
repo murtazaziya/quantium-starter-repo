0c61cd294430a4a22405b2a8e9c4459a9256c671
Murtaza Ziya <ziya.murtaza141@gmail.com>
2024-06-26 19:25:29 -0400

    feat: added sales visualization

---
+++ src/app.py
@@ -0,0 +1,50 @@
+    # Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+
+from dash import Dash, dcc, html
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv('data/data.csv')
+
+fig = px.line(df, x="date", y="sales", title="Sales over the years")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Soul Foods',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Were sales higher before or after the Pink Morsel price increase on the 15th of January, 2021?', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
